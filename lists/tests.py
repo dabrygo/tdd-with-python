69b77e3360eef05d50f2148ee2ed63c64b28a3fd
Daniel <dbgodfrey89@msn.com>
2019-12-08 21:42:27 -0700

    Model allows for more than one list on site

--- lists/tests.py
+++ lists/tests.py
@@ -3,20 +3,8 @@ from django.test import TestCase
 from django.template.loader import render_to_string
 from django.http import HttpRequest
 
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
-
-
-class HomePageTest(TestCase):
-  def test_root_url_resolves_to_home_page_view(self):
-    found = resolve('/')
-    self.assertEqual(found.func, home_page)
-
-  def test_home_page_returns_correct_html(self):
-    request = HttpRequest()
-    response = home_page(request)
-    expected_html = render_to_string('home.html')
-    self.assertEqual(response.content.decode(), expected_html)
 
 
 class NewListTest(TestCase):
@@ -39,31 +27,48 @@ class NewListTest(TestCase):
 
 
 class ListViewTest(TestCase):
+  def test_root_url_resolves_to_home_page_view(self):
+    found = resolve('/')
+    self.assertEqual(found.func, home_page)
+
+  def test_home_page_returns_correct_html(self):
+    request = HttpRequest()
+    response = home_page(request)
+    expected_html = render_to_string('home.html')
+    self.assertEqual(response.content.decode(), expected_html)
+
   def test_displays_all_items(self):
-    Item.objects.create(text='itemey 1')
-    Item.objects.create(text='itemey 2')
+    list_ = List.objects.create()
+    Item.objects.create(text='itemey 1', list=list_)
+    Item.objects.create(text='itemey 2', list=list_)
 
     response = self.client.get('/lists/the-only-list-in-the-world/')
 
     self.assertIn('itemey 1', response.content.decode())
     self.assertIn('itemey 2', response.content.decode())
 
-
-class ListViewTest(TestCase):
   def test_uses_list_template(self):
     response = self.client.get('/lists/the-only-list-in-the-world/')
     self.assertTemplateUsed(response, 'list.html')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
   def test_saving_and_retrieving_items(self):
+    list_ = List()
+    list_.save()
+
     first_item = Item()
     first_item.text = 'The first (ever) list item'
+    first_item.list = list_
     first_item.save()
 
     second_item = Item()
     second_item.text = 'Item the second'
+    second_item.list = list_
     second_item.save()
+
+    saved_list = List.objects.first()
+    self.assertEqual(saved_list, list_)
 
     saved_items = Item.objects.all()
     self.assertEqual(saved_items.count(), 2)
@@ -71,4 +76,6 @@ class ItemModelTest(TestCase):
     first_saved_item = saved_items[0]
     second_saved_item = saved_items[1]
     self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+    self.assertEqual(first_saved_item.list, list_)
     self.assertEqual(second_saved_item.text, 'Item the second')
+    self.assertEqual(second_saved_item.list, list_)
